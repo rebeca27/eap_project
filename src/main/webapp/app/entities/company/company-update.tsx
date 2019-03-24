d599f5c0ad327dae0b2195a4b8476007a93a9503
Unknown <rebekaoprea@yahoo.com>
2019-03-24 17:05:05 +0200

    Added company class

---
+++ src/main/webapp/app/entities/company/company-update.tsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { Button, Row, Col, Label } from 'reactstrap';
+import { AvForm, AvGroup, AvInput, AvField } from 'availity-reactstrap-validation';
+// tslint:disable-next-line:no-unused-variable
+import { ICrudGetAction, ICrudGetAllAction, ICrudPutAction } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IRootState } from 'app/shared/reducers';
+
+import { getEntity, updateEntity, createEntity, reset } from './company.reducer';
+import { ICompany } from 'app/shared/model/company.model';
+// tslint:disable-next-line:no-unused-variable
+import { convertDateTimeFromServer, convertDateTimeToServer } from 'app/shared/util/date-utils';
+import { mapIdList } from 'app/shared/util/entity-utils';
+
+export interface ICompanyUpdateProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
+
+export interface ICompanyUpdateState {
+  isNew: boolean;
+}
+
+export class CompanyUpdate extends React.Component<ICompanyUpdateProps, ICompanyUpdateState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNew: !this.props.match.params || !this.props.match.params.id
+    };
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.updateSuccess !== this.props.updateSuccess && nextProps.updateSuccess) {
+      this.handleClose();
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.isNew) {
+      this.props.reset();
+    } else {
+      this.props.getEntity(this.props.match.params.id);
+    }
+  }
+
+  saveEntity = (event, errors, values) => {
+    if (errors.length === 0) {
+      const { companyEntity } = this.props;
+      const entity = {
+        ...companyEntity,
+        ...values
+      };
+
+      if (this.state.isNew) {
+        this.props.createEntity(entity);
+      } else {
+        this.props.updateEntity(entity);
+      }
+    }
+  };
+
+  handleClose = () => {
+    this.props.history.push('/entity/company');
+  };
+
+  render() {
+    const { companyEntity, loading, updating } = this.props;
+    const { isNew } = this.state;
+
+    return (
+      <div>
+        <Row className="justify-content-center">
+          <Col md="8">
+            <h2 id="eapProjectApp.company.home.createOrEditLabel">Create or edit a Company</h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md="8">
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <AvForm model={isNew ? {} : companyEntity} onSubmit={this.saveEntity}>
+                {!isNew ? (
+                  <AvGroup>
+                    <Label for="id">ID</Label>
+                    <AvInput id="company-id" type="text" className="form-control" name="id" required readOnly />
+                  </AvGroup>
+                ) : null}
+                <AvGroup>
+                  <Label id="nameLabel" for="name">
+                    Name
+                  </Label>
+                  <AvField
+                    id="company-name"
+                    type="text"
+                    name="name"
+                    validate={{
+                      required: { value: true, errorMessage: 'This field is required.' },
+                      minLength: { value: 1, errorMessage: 'This field is required to be at least 1 characters.' },
+                      maxLength: { value: 128, errorMessage: 'This field cannot be longer than 128 characters.' }
+                    }}
+                  />
+                </AvGroup>
+                <AvGroup>
+                  <Label id="cuiLabel" for="cui">
+                    Cui
+                  </Label>
+                  <AvField
+                    id="company-cui"
+                    type="text"
+                    name="cui"
+                    validate={{
+                      required: { value: true, errorMessage: 'This field is required.' },
+                      minLength: { value: 9, errorMessage: 'This field is required to be at least 9 characters.' },
+                      maxLength: { value: 9, errorMessage: 'This field cannot be longer than 9 characters.' }
+                    }}
+                  />
+                </AvGroup>
+                <Button tag={Link} id="cancel-save" to="/entity/company" replace color="info">
+                  <FontAwesomeIcon icon="arrow-left" />
+                  &nbsp;
+                  <span className="d-none d-md-inline">Back</span>
+                </Button>
+                &nbsp;
+                <Button color="primary" id="save-entity" type="submit" disabled={updating}>
+                  <FontAwesomeIcon icon="save" />
+                  &nbsp; Save
+                </Button>
+              </AvForm>
+            )}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (storeState: IRootState) => ({
+  companyEntity: storeState.company.entity,
+  loading: storeState.company.loading,
+  updating: storeState.company.updating,
+  updateSuccess: storeState.company.updateSuccess
+});
+
+const mapDispatchToProps = {
+  getEntity,
+  updateEntity,
+  createEntity,
+  reset
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CompanyUpdate);
